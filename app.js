aa583f68dc29102d46ae817bd4fdd38e98641dd9
Hayk <hsimonyan09@gmail.com>
2019-03-21 19:11:40 +0400

    database

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect("mongodb://localhost/cat_app");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
