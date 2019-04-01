7ed891b28d994f1eb3464d3accee657e205eab6b
Hayk <hsimonyan09@gmail.com>
2019-04-01 23:10:05 +0400

    V4.1

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp_4", { useNewUrlParser: tru
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
-console.log(__dirname);
 seedDB();
 
 app.get("/", function(req, res) {
